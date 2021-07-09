5668ecc719ead155f1f77c419d6d9f44fd9977a0
jckbb <josephkbburghard@gmail.com>
2021-07-09 09:52:03 -0700

    Add text field component with field label, text input and styling

---
+++ src/screens/Brew/components/TextField/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+import styles from './styles';
+
+const TextField = (props) => {
+  return (
+    <View style={[props.hasTopRoom && styles.topSpacing]}>
+      <Text style={styles.label}>{props.label}</Text>
+      <TextInput
+        style={styles.textbox}
+        placeholder={props.placeholder}
+        value={props.value ? props.value : ''}
+        onChangeText={props.onChangeText}
+      />
+    </View>
+  );
+};
+
+export default TextField;
