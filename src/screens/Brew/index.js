721f6f342d253cf5ca6e573957f76a536e1c2842
jckbb <josephkbburghard@gmail.com>
2021-07-09 09:53:24 -0700

    Update form wrapper child with text field, handle state with hook and validation check on form submit

--- src/screens/Brew/index.js
+++ src/screens/Brew/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView} from 'react-native';
 
 import FormWrapper from './components/FormWrapper';
+import TextField from './components/TextField';
 
 const Brew = (props) => {
+  const [roastName, setRoastName] = useState('');
+
   const handleSubmit = () => {
+    // quick valid check for text field fill
+    if (roastName.length <= 0) return;
+
+    // close modal
     props.onRequestClose();
   };
 
   return (
     <ScrollView>
       <FormWrapper onSubmit={handleSubmit}>
+        <TextField
+          hasTopRoom
+          label={'Roast Name'}
+          placeholder={'Enter roast name'}
+          value={roastName}
+          onChangeText={(text) => {
+            setRoastName(text);
+          }}
+        />
       </FormWrapper>
     </ScrollView>
   );
