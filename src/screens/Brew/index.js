955f4af5be7b1c4731406144caeffa27b28cb88c
jckbb <josephkbburghard@gmail.com>
2021-07-12 09:33:47 -0700

    Update render with bottom drawer, option list, button fields for brew and grinder. Also handle components

--- src/screens/Brew/index.js
+++ src/screens/Brew/index.js
@@ -1,13 +1,30 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 import {ScrollView} from 'react-native';
 
+import {
+  grinderModelData,
+  grinderModelByName,
+  brewModelByName,
+  brewModelData,
+} from '../../common/data';
+import BottomDrawer, {drawerState} from '../../common/components/BottomDrawer';
 import FormWrapper from './components/FormWrapper';
 import TextField from './components/TextField';
 import ButtonField from './components/ButtonField';
+import OptionList from './components/OptionList';
 
 import formReducer, {actionTypes, initialState} from './reducer';
+import styles from './styles';
+
+const initialOptions = {
+  type: undefined,
+  data: {},
+  names: [],
+};
 
 const Brew = (props) => {
+  const [options, setOptions] = useState(initialOptions);
+  const [isBottomDrawerVisible, setBottomDrawerVisible] = useState(false);
   const [{data, error}, dispatch] = useReducer(formReducer, initialState);
 
   const updateField = (field, payload) => {
@@ -45,29 +62,74 @@ const Brew = (props) => {
     props.onRequestClose();
   };
 
+  const handleBottomDrawerClose = () => {
+    setBottomDrawerVisible(false);
+    setOptions(initialOptions);
+  };
+
   return (
-    <ScrollView>
-      <FormWrapper onSubmit={handleSubmit}>
-        <TextField
-          hasTopRoom
-          error={error.roastName}
-          label={'Roast Name'}
-          placeholder={'Enter roast name'}
-          value={data.roastName}
-          onChangeText={(text) => {
-            updateField('roastName', text);
+    <>
+      <ScrollView style={styles.brew}>
+        <FormWrapper onSubmit={handleSubmit}>
+          <TextField
+            hasTopRoom
+            error={error.roastName}
+            label={'Roast Name'}
+            placeholder={'Enter roast name'}
+            value={data.roastName}
+            onChangeText={(text) => {
+              updateField('roastName', text);
+            }}
+          />
+          <ButtonField
+            hasTopRoom
+            error={error.brewMethod}
+            label={'Brew Method'}
+            value={data.brewMethod}
+            placeholder={'Select brew method'}
+            onPress={() => {
+              setOptions({
+                type: 'brewMethod',
+                data: brewModelData,
+                names: brewModelByName,
+              });
+              setBottomDrawerVisible(true);
+            }}
+          />
+          <ButtonField
+            hasTopRoom
+            error={error.grinder}
+            label={'Grinder'}
+            value={data.grinder}
+            placeholder={'Select grinder'}
+            onPress={() => {
+              setOptions({
+                type: 'grinder',
+                data: grinderModelData,
+                names: grinderModelByName,
+              });
+              setBottomDrawerVisible(true);
+            }}
+          />
+        </FormWrapper>
+      </ScrollView>
+      <BottomDrawer
+        isVisible={isBottomDrawerVisible}
+        onDrawerStateChange={(changedState) => {
+          if (drawerState.Closed === changedState) {
+            handleBottomDrawerClose();
+          }
+        }}>
+        <OptionList
+          optionData={options.data}
+          options={options.names}
+          onOptionChange={selectedOption => {
+            updateField(options.type, selectedOption);
+            handleBottomDrawerClose();
           }}
         />
-        <ButtonField
-          hasTopRoom
-          error={error.brewMethod}
-          label={'Brew Method'}
-          value={data.brewMethod}
-          placeholder={'Enter brew method'}
-          onPress={() => {}}
-        />
-      </FormWrapper>
-    </ScrollView>
+      </BottomDrawer>
+    </>
   );
 };
 
