eb93422923e37e1c91f7b422c1b42f6f66363686
jckbb <josephkbburghard@gmail.com>
2021-07-12 14:18:48 -0700

    Add slider with label, animated tracker, bounds and styling

---
+++ src/screens/Brew/components/SliderField/index.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {Animated, View, Text} from 'react-native';
+import Slider from '@react-native-community/slider';
+import styles, {minTrackerTint, maxTrackerTint} from './styles';
+
+const SliderField = ({label, ...props}) => {
+  const animatedValue = useRef(new Animated.Value(props.value)).current;
+  const [sliderValue, setSliderValue] = useState(0);
+  const [sliderWidth, setSliderWidth] = useState(0);
+
+  useEffect(() => {
+    animateSlider(sliderValue);
+    props.onValueChange(sliderValue);
+  }, [sliderValue]);
+
+  const animateSlider = (value) => {
+    animatedValue.setValue(value / props.maxDial);
+  };
+
+  return (
+    <View style={[props.hasTopRoom && styles.topSpacing]}>
+      <Text style={styles.label}>{label}</Text>
+      {props.shouldHideSlider ? <View style={styles.sliderSpace}/> : (
+        <>
+          <Animated.View
+            style={[
+              styles.floatTracker,
+              {
+                transform: [
+                  {
+                    translateX: animatedValue.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [8, sliderWidth - 20],
+                    }),
+                  },
+                ],
+              },
+            ]}>
+            <Text style={styles.trackerText}>{props.value}</Text>
+          </Animated.View>
+          <Slider
+            onLayout={(event) => {
+              setSliderWidth(event.nativeEvent.layout.width);
+            }}
+            style={styles.slider}
+            minimumValue={props.minDial}
+            maximumValue={props.maxDial}
+            step={props.step}
+            minimumTrackTintColor={minTrackerTint}
+            maximumTrackTintColor={maxTrackerTint}
+            value={props.value}
+            onValueChange={(value) => {
+              setSliderValue(value);
+            }}
+          />
+          <View style={styles.bounds}>
+            <Text style={styles.boundsText}>{props.minDial}</Text>
+            <Text style={styles.boundsText}>{props.maxDial}</Text>
+          </View>
+        </>
+      )}
+    </View>
+  );
+};
+
+export default SliderField;
---
+++ src/screens/Brew/components/SliderField/styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from 'react-native';
+import {WHITE} from '../../../../common/res/colors';
+
+export const maxTrackerTint = 'rgba(255, 255, 255, 0.6)';
+export const minTrackerTint = WHITE;
+
+export default StyleSheet.create({
+  topSpacing: {
+    marginTop: 20,
+  },
+  slider: {
+    marginTop: 15,
+    width: '100%',
+    height: 50,
+  },
+  sliderSpace: {
+    height: 50,
+  },
+  trackerText: {
+    color: WHITE,
+    fontWeight: '700',
+    fontSize: 18,
+  },
+  floatTracker: {
+    alignItems: 'center',
+    // backgroundColor:'yellow',
+    top: 25,
+    position: 'absolute',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '500',
+    letterSpacing: 0.2,
+    color: WHITE,
+    marginBottom: 3,
+  },
+  bounds: {
+    marginHorizontal: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  boundsText: {
+    color: maxTrackerTint,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
