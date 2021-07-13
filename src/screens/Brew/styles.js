8eb55b0a21189f0154eb8c778266c88662e6563a
jckbb <josephkbburghard@gmail.com>
2021-07-13 11:14:50 -0700

    Update brew form styling

--- src/screens/Brew/styles.js
+++ src/screens/Brew/styles.js
@@ -1,3 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  brew: {
+    flex: 1,
+    marginVertical: 80,
+  },
+  formRow: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+});
