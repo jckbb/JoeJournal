04fb04ef1da0e5a34149340dce8b9a87761a74d0
jckbb <josephkbburghard@gmail.com>
2021-07-13 11:11:04 -0700

    Add field to handle number and unit specific measurements including label and styling

---
+++ src/screens/Brew/components/NumberField/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+import {WHITE} from '../../../../common/res/colors';
+
+export default StyleSheet.create({
+  textbox: {
+    fontSize: 20,
+    fontWeight: '500',
+    borderRadius: 7,
+    height: 50,
+    paddingLeft: 8,
+    backgroundColor: WHITE,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '500',
+    letterSpacing: 0.2,
+    color: WHITE,
+    marginBottom: 3,
+  },
+  topSpacing: {
+    marginTop: 20,
+  },
+});
