a19e8af1d4178c2fd13dc1e38b1ebe9b58c06fe6
jckbb <josephkbburghard@gmail.com>
2021-07-15 10:50:01 -0700

    Update read all logs and render list of logs

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {TouchableOpacity, Text, View, FlatList} from 'react-native';
 import HoverButton from '../../common/components/HoverButton';
 import ModalWrapper from '../../common/components/ModalWrapper';
+import {readLogs} from '../../storage';
 
 import Brew from '../Brew';
 import styles from './styles';
 
 const Home = () => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [log, setLog] = useState({
+    data: null,
+    keys: [],
+  });
+
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  const getLogs = async () => {
+    setLog(await readLogs());
+  };
 
   const handleBrewPress = () => {
     setModalVisible(true);
@@ -17,9 +30,23 @@ const Home = () => {
     setModalVisible(false);
   };
 
+  const renderItem = ({item, index}) => {
+    return (
+      <TouchableOpacity style={styles.logButton} onPress={() => {}}>
+        <Text>{new Date(log.data[item].createdAt).toISOString()}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.home}>
-      <Text>{'home'}</Text>
+      <FlatList
+        style={{flex: 1}}
+        data={log.keys}
+        renderItem={renderItem}
+        keyExtractor={(__, index) => index.toString()}
+        ItemSeparatorComponent={() => <View style={styles.divider} />}
+      />
       <ModalWrapper
         visible={isModalVisible}
         onRequestClose={handleRequestClose}>
