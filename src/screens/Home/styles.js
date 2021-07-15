3acea73b5751acecfea044ac4e2a7926b17f2ea9
jckbb <josephkbburghard@gmail.com>
2021-07-15 10:50:16 -0700

    Update styling

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,9 +1,17 @@
 import {StyleSheet} from 'react-native';
+import {PRIMARY_COLOR} from '../../common/res/colors';
 
 export default StyleSheet.create({
   home: {
     flex: 1,
+    marginTop: 35,
+  },
+  logButton: {
+    height: 50,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: PRIMARY_COLOR,
   },
 });
