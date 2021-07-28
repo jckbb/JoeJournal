69006dd0b5c96211ca0557db0b33fc3eef0e7d8d
jckbb <josephkbburghard@gmail.com>
2021-07-28 14:25:09 -0700

    Update state structure

--- src/screens/Brew/data/reducer.js
+++ src/screens/Brew/data/reducer.js
@@ -1,143 +1,93 @@
 import {actionTypes} from './actions';
 
 const initialSplitState = {
-  stage: undefined,
-  duration: 0,
-  waterAmount: 0,
-};
-
-const initialErrorSplitState = {
-  stageName: {
-    hasError: false,
+  stage: {
+    hasError: true,
+    error: '',
+    value: '',
   },
   duration: {
-    hasError: false,
+    hasError: true,
+    error: '',
+    value: 0,
   },
   waterAmount: {
-    hasError: false,
+    hasError: true,
+    error: '',
+    value: 0,
   },
 };
 
 export const initialState = {
-  error: {
-    roaster: {
-      hasError: false,
-    },
-    brewMethod: {
-      hasError: false,
-    },
-    grinder: {
-      hasError: false,
-    },
-    dial: {
-      hasError: false,
-    },
-    waterTemperature: {
-      hasError: false,
-    },
-    waterAmount: {
-      hasError: false,
-    },
-    coffeeAmount: {
-      hasError: false,
-    },
-    region: {
-      hasError: false,
-    },
-    split_0: initialErrorSplitState,
+  roaster: {
+    hasError: true,
+    error: '',
+    value: '',
   },
-  data: {
-    roaster: undefined,
-    region: undefined,
-    brewMethod: undefined,
-    grinder: undefined,
-    dial: 0,
-    coffeeAmount: 0,
-    waterTemperature: 0,
-    splitData: {
-      split_0: initialSplitState,
-    },
-    splitByIndex: ['split_0'],
+  region: {
+    hasError: true,
+    error: '',
+    value: '',
   },
+  brewMethod: {
+    hasError: true,
+    error: '',
+    value: '',
+  },
+  grinder: {
+    hasError: true,
+    error: '',
+    value: undefined,
+  },
+  dial: {
+    hasError: true,
+    error: '',
+    value: 0,
+  },
+  coffeeAmount: {
+    hasError: true,
+    error: '',
+    value: 0,
+  },
+  waterTemperature: {
+    hasError: true,
+    error: '',
+    value: 0,
+  },
+  brewSplits: [initialSplitState],
 };
 
 const formReducer = (state, action) => {
   switch (action.type) {
-    case actionTypes.UPDATE_ERROR:
-      return {
-        ...state,
-        error: {
-          ...state.error,
-          [action.field]: {
-            hasError: action.payload,
-          },
-        },
-      };
     case actionTypes.UPDATE_FIELD:
       return {
         ...state,
-        error: {
-          ...state.error,
-          [action.field]: {
-            hasError: false,
-          },
-        },
-        data: {
-          ...state.data,
-          [action.field]: action.payload,
-        },
+        [action.field]: action.payload,
       };
     case actionTypes.ADD_SPLIT_ITEM:
-      const index = state.data.splitByIndex.length;
-      const splitKey = `split_${index}`;
-
       return {
         ...state,
-        error: {
-          ...state.error,
-          [splitKey]: initialErrorSplitState,
-        },
-        data: {
-          ...state.data,
-          splitData: {
-            ...state.data.splitData,
-            [splitKey]: initialSplitState,
-          },
-          splitByIndex: [...state.data.splitByIndex, splitKey],
-        },
+        brewSplits: [...state.brewSplits, initialSplitState],
       };
     case actionTypes.DELETE_SPLIT_ITEM:
-      const {[action.payload]: __, ...mutatedSplitData} = {
-        ...state.data.splitData,
-      };
-      const {[action.payload]: poppedError, ...mutatedError} = {
-        ...state.error,
-      };
-      const mutatedSplitsByIndex = [...state.data.splitByIndex];
-      mutatedSplitsByIndex.pop();
+      const mutatedBrewSplits = [...state.brewSplits];
+      mutatedBrewSplits.pop();
 
       return {
         ...state,
-        error: mutatedError,
-        data: {
-          ...state.data,
-          splitData: mutatedSplitData,
-          splitByIndex: mutatedSplitsByIndex,
-        },
+        brewSplits: mutatedBrewSplits,
       };
     case actionTypes.UPDATE_SPLIT_FIELD:
       return {
         ...state,
-        data: {
-          ...state.data,
-          splitData: {
-            ...state.data.splitData,
-            [action.key]: {
-              ...state.data.splitData[action.key],
-              [action.field]: action.payload,
-            },
-          },
-        },
+        brewSplits: [...state.brewSplits].map((data, index) => {
+          if (action.index !== index) return data;
+
+          return {
+            ...data,
+            [action.field]: action.payload,
+          };
+        }),
       };
     default:
       return state;
