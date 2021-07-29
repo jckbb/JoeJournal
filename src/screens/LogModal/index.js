5bfa9492bbd49b9d902efb8600e17d3537225631
jckbb <josephkbburghard@gmail.com>
2021-07-29 11:27:25 -0700

    Update styling and render brew splits as a table and handle null prop data

--- src/screens/LogModal/index.js
+++ src/screens/LogModal/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ScrollView, View, Text} from 'react-native';
 import ModalWrapper from '../../common/components/ModalWrapper';
 import {unitType} from '../../common/res/strings';
 
+import {fromTimestampToReadableDate} from './utils';
 import styles from './styles';
 
 const LogModal = (props) => {
+  useEffect(() => {
+    if (!props.data) props.onRequestClose();
+  }, []);
+
   const renderDetail = (label, value) => (
     <View style={[styles.row, styles.detail]}>
       <Text style={styles.labelText}>{`${label}:`}</Text>
@@ -14,32 +19,62 @@ const LogModal = (props) => {
   );
 
   const renderSplitDetail = (item, index) => (
-    <View key={index} style={styles.section}>
-      {renderDetail('Stage', item.stage)}
-      {renderDetail('Duration', `${item.duration}${unitType.seconds}`)}
-      {renderDetail('Water Amount', `${item.waterAmount}${unitType.gram}`)}
+    <View key={index} style={styles.brewSplit}>
+      <View style={styles.column}>
+        <Text style={styles.detailText}>{item.stage}</Text>
+      </View>
+      <View style={styles.column}>
+        <Text style={styles.detailText}>
+          {`${item.waterAmount}${unitType.gram}`}
+        </Text>
+      </View>
+      <View style={styles.column}>
+        <Text style={styles.detailText}>
+          {`${item.duration}${unitType.seconds}`}
+        </Text>
+      </View>
+    </View>
+  );
+
+  const renderBrewSplitsHeader = () => (
+    <View style={styles.brewSplitsHeader}>
+      <View style={styles.column}>
+        <Text style={styles.labelText}>{'stage'}</Text>
+      </View>
+      <View style={styles.column}>
+        <Text style={styles.labelText}>{'water amount'}</Text>
+      </View>
+      <View style={styles.column}>
+        <Text style={styles.labelText}>{'duration'}</Text>
+      </View>
     </View>
   );
 
   return (
-    <ModalWrapper
-      visible={props.isVisible}
-      onRequestClose={props.onRequestClose}>
-      <ScrollView contentContainerStyle={styles.log}>
-        <Text style={styles.title}>{'06/10/2021'}</Text>
-        <View style={styles.section}>
-          {renderDetail('Roast', 'Brazilian Dark Roast')}
-          {renderDetail('Brew Method', 'Hario V60 pour-over')}
-          {renderDetail('Grinder', 'Encore')}
-        </View>
-        <View style={styles.section}>
-          {renderDetail('Grind Size', 20)}
-          {renderDetail('Coffee Amount', `15${unitType.gram}`)}
-          {renderDetail('Water Amount', `250${unitType.gram}`)}
-        </View>
-        {[{stage: 'Bloom', duration: 60, waterAmount: 40}].map(renderSplitDetail)}
-      </ScrollView>
-    </ModalWrapper>
+    props.data && (
+      <ModalWrapper
+        visible={props.isVisible}
+        onRequestClose={props.onRequestClose}>
+        <ScrollView contentContainerStyle={styles.log}>
+          <View style={styles.generalContainer}>
+            <Text style={styles.title}>
+              {fromTimestampToReadableDate(props.data.createdAt)}
+            </Text>
+            {renderDetail('Roaster', props.data.roaster)}
+            {renderDetail('Region', props.data.region)}
+            {renderDetail('Grinder', props.data.grinder)}
+            {renderDetail('Brew Method', props.data.brewMethod)}
+          </View>
+          <View style={styles.brewContainer}>
+            {renderDetail('Grind Size', 20)}
+            {renderDetail('Total Coffee Amount', `15${unitType.gram}`)}
+            {renderDetail('Total Water Amount', `250${unitType.gram}`)}
+            {renderBrewSplitsHeader()}
+            {props.data.brewSplits.map(renderSplitDetail)}
+          </View>
+        </ScrollView>
+      </ModalWrapper>
+    )
   );
 };
 
