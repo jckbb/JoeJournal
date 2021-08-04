40763072fa1283763343831fceea2e058605da16
jckbb <josephkbburghard@gmail.com>
2021-08-04 08:54:33 -0700

    Move create brew record func closer to origin and include brew split totals

--- src/screens/Brew/utils.js
+++ src/screens/Brew/utils.js
@@ -42,3 +42,41 @@ export const validateForm = (data) => {
 
   return isFormValid;
 };
+
+export const createBrewRecord = (data, totals) => {
+  let record = {
+    brewSplits: [],
+  };
+
+  for (const key of Object.keys(data)) {
+    switch (key) {
+      case 'brewSplits':
+        data[key].forEach((field, __) => {
+          const brewSplit = {
+            duration: field.duration.value,
+            stage: field.stage.value,
+            waterAmount: field.waterAmount.value,
+          };
+
+          record = {
+            ...record,
+            brewSplits: [...record.brewSplits, brewSplit],
+          };
+        });
+        break;
+      default:
+        const field = data[key];
+        record = {
+          ...record,
+          [key]: field.value,
+        };
+        break;
+    }
+  }
+
+  return {
+    ...record,
+    ...totals,
+    createdAt: new Date().getTime(),
+  };
+};
