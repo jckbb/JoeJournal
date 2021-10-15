d161176214e8359819fcd4f170e7d748a90894d2
jckbb <josephkbburghard@gmail.com>
2021-10-14 17:33:34 -0700

    Update app colors

--- src/common/res/colors.js
+++ src/common/res/colors.js
@@ -1,4 +1,24 @@
-export const PRIMARY_COLOR = '#c0392b';
 export const BLACK = '#000000';
 export const WHITE = '#ffffff';
-export const IVORY_WHITE = '#fffff4';
+
+// ivory white
+export const PRIMARY_COLOR_100 = '#fffffd';
+export const PRIMARY_COLOR_200 = '#fffffb';
+export const PRIMARY_COLOR_300 = '#fffff8';
+export const PRIMARY_COLOR_400 = '#fffff6';
+export const PRIMARY_COLOR_500 = '#fffff4';
+export const PRIMARY_COLOR_600 = '#ccccc3';
+export const PRIMARY_COLOR_700 = '#999992';
+export const PRIMARY_COLOR_800 = '#666662';
+export const PRIMARY_COLOR_900 = '#333331';
+
+// burgandy red
+export const SECONDARY_COLOR_100 = '#f2d7d5';
+export const SECONDARY_COLOR_200 = '#e6b0aa';
+export const SECONDARY_COLOR_300 = '#d98880';
+export const SECONDARY_COLOR_400 = '#cd6155';
+export const SECONDARY_COLOR_500 = '#c0392b';
+export const SECONDARY_COLOR_600 = '#9a2e22';
+export const SECONDARY_COLOR_700 = '#73221a';
+export const SECONDARY_COLOR_800 = '#4d1711';
+export const SECONDARY_COLOR_900 = '#260b09';
