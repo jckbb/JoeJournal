6f70ec83541ca43f13a5cfb04ed9a2d682e4cadf
jckbb <josephkbburghard@gmail.com>
2021-10-21 10:13:39 -0700

    Add stage form component

---
+++ src/screens/StageForm/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+/**
+ * STAGES
+ * water amount poured
+ * pour duration
+ * wait duration
+ * description (optional)
+ */
+
+const StageForm = () => {
+  return (
+    <View>
+      <Text>{'STAGE'}</Text>
+    </View>
+  );
+};
+
+export default StageForm;
