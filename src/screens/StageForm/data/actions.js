d0735f710eb91e80e6ad09f3d2497b7a621f486a
jckbb <josephkbburghard@gmail.com>
2021-10-24 17:59:38 -0700

    Update

---
+++ src/screens/StageForm/data/actions.js
@@ -0,0 +1,28 @@
+export const actionTypes = {
+  UPDATE_FIELD: 'update field',
+  ADD_STAGE: 'add stage',
+  REMOVE_STAGE: 'remove stage',
+  UPDATE_SELECTED_STAGE: 'update selected stage',
+};
+
+export const updateSelectedStage = (id) => ({
+  type: actionTypes.UPDATE_SELECTED_STAGE,
+  payload: id,
+});
+
+export const updateField = (id, name, data) => ({
+  type: actionTypes.UPDATE_FIELD,
+  stageId: id,
+  field: name,
+  error: data === undefined || data.length <= 0,
+  payload: data,
+});
+
+export const addStage = () => ({
+  type: actionTypes.ADD_STAGE,
+});
+
+export const removeStage = (id) => ({
+  type: actionTypes.REMOVE_STAGE,
+  stageId: id,
+});
