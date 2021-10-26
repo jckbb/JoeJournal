d889bfc5dfa1a10b1bcd4fe1f78847104a499769
jckbb <josephkbburghard@gmail.com>
2021-10-26 13:15:37 -0700

    Add info svg to resources

--- src/common/res/svgs/index.js
+++ src/common/res/svgs/index.js
@@ -2,3 +2,4 @@ export {default as EditIcon} from './EditIcon.svg';
 export {default as CloseIcon} from './CloseIcon.svg';
 export {default as PlusSvg} from './Plus.svg';
 export {default as ArrowRightSvg} from './ArrowRight.svg';
+export {default as InfoSvg} from './Info.svg';
