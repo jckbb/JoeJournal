11f0b5fd8803cf4f359d0fcd190f872d10704e31
jckbb <josephkbburghard@gmail.com>
2021-10-26 14:16:00 -0700

    Update component styling

--- src/screens/PrepForm/styles.js
+++ src/screens/PrepForm/styles.js
@@ -1,9 +1,24 @@
 import {StyleSheet} from 'react-native';
-import {WHITE} from '../../common/res/colors';
+import {PRIMARY_COLOR_500} from '../../common/res/colors';
 
 export default StyleSheet.create({
   form: {
     flex: 1,
     marginHorizontal: '10%',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  ratio: {
+    marginTop: 20,
+    alignItems: 'flex-end',
+  },
+  ratioText: {
+    color: PRIMARY_COLOR_500,
+    fontSize: 25,
+    fontWeight: '700',
+    letterSpacing: 0.2,
+    marginLeft: 10,
+    marginBottom: 9,
   },
 });
