00372d534723720a5d8a11f0a8f61edded24e7fe
jckbb <josephkbburghard@gmail.com>
2021-10-27 16:09:46 -0700

    Add Water and Timer svg icons

--- src/common/res/svgs/index.js
+++ src/common/res/svgs/index.js
@@ -3,3 +3,5 @@ export {default as CloseIcon} from './CloseIcon.svg';
 export {default as PlusSvg} from './Plus.svg';
 export {default as ArrowRightSvg} from './ArrowRight.svg';
 export {default as InfoSvg} from './Info.svg';
+export {default as WaterSvg} from './Water.svg';
+export {default as TimerSvg} from './Timer.svg';
