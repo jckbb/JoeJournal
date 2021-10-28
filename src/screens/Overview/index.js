ef6bae6cee05a5f909619c526d006ac4f95cbb03
jckbb <josephkbburghard@gmail.com>
2021-10-28 13:34:49 -0700

    Add Overview component and render series of paginatable questions with multi choice anwsers

---
+++ src/screens/Overview/index.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+import Title from '../../common/components/Title';
+import Step from '../../common/components/Step';
+
+import {overviewQuestions} from './res/strings';
+import styles from './styles';
+
+const Overview = (props) => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const handleComplete = () => {
+    props.onNavigateTo('home');
+  };
+
+  const renderQuestionSection = (item, index) => (
+    <View key={index}>
+      <View style={styles.question}>
+        <Text style={styles.questionText}>{item.question}</Text>
+      </View>
+      <View>
+        {item.options.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.option}
+            onPress={() => {
+              if (questionIndex >= overviewQuestions.length - 1) {
+                handleComplete();
+                return;
+              }
+              setQuestionIndex((prev) => prev + 1);
+            }}>
+            <Text style={styles.optionText}>{item.answer}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={styles.overview}>
+      <Title>{'Overview'}</Title>
+      <Step
+        totalSteps={overviewQuestions.length}
+        currentStep={questionIndex + 1}
+      />
+      {renderQuestionSection(overviewQuestions[questionIndex], 0)}
+    </View>
+  );
+};
+
+export default Overview;
---
+++ src/screens/Overview/styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+import {
+  SECONDARY_COLOR_900,
+  PRIMARY_COLOR_500,
+  SECONDARY_COLOR_600,
+  SECONDARY_COLOR_100,
+} from '../../common/res/colors';
+
+export default StyleSheet.create({
+  overview: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  question: {
+    padding: 10,
+  },
+  questionText: {
+    fontSize: 25,
+    fontWeight: '700',
+    letterSpacing: 0.2,
+    color: PRIMARY_COLOR_500,
+  },
+  option: {
+    backgroundColor: SECONDARY_COLOR_600,
+    borderRadius: 7,
+    padding: 10,
+    marginTop: 8,
+  },
+  optionText: {
+    color: SECONDARY_COLOR_100,
+    fontSize: 20,
+  },
+});
